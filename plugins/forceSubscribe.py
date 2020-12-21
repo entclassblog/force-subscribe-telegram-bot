edf12a6e92b765ebac88b7c09322600c8922276e
Abir Hasan <hackitabirhasan@gmail.com>
2020-12-21 23:11:25 +0600

    Update forceSubscribe.py

--- plugins/forceSubscribe.py
+++ plugins/forceSubscribe.py
@@ -49,11 +49,12 @@ def _check_member(client, message):
         client.get_chat_member(channel, user_id)
       except UserNotParticipant:
         try:
+          channel = chat_db.channel
           sent_message = message.reply_text(
               "{}, You Are **Not Subscribed** To My [CHANNEL 📢](https://t.me/{}) Yet.\n\nPlease [JOIN](https://t.me/{}) & **Press The Button Below** to unmute yourself.".format(message.from_user.mention, channel, channel),
               disable_web_page_preview=True,
               reply_markup=InlineKeyboardMarkup(
-                  [[InlineKeyboardButton("📢 Join Channel 📢", url="https://t.me/{}")]],
+                  [[InlineKeyboardButton("📢 Join Channel 📢", url=f"https://t.me/{channel}")]],
                   [[InlineKeyboardButton("🗣 UNMUTE ME 🗣", callback_data="onUnMuteRequest")]]
               )
           )
