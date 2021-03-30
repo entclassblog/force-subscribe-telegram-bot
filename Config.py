debf25cac6ee64c5e7ced2d328cbaea1cc2ed366
女神 <53878781+RomanGoddess@users.noreply.github.com>
2021-03-30 06:49:58 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,7 +12,7 @@ class Config():
     SUDO_USERS = list(set(SUDO_USERS))
   else:
     BOT_TOKEN = "1495982554:AAF3m6ccvtVxl6f-McE2zDsm3WWPcjOli4k"
-    DATABASE_URL = "postgres://imhlhaozesitgn:dcf13aec8813600086d3a657baeb26e89eb1694072adaa83af09a21463c4e0ae@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/d647tbgp648fe8"
+    DATABASE_URL = "postgres://uvfklnpyfwvhas:9ea39b97e591ca74ff176c02139658e25516fd7340c1a5324113a6b9f0843794@ec2-34-254-69-72.eu-west-1.compute.amazonaws.com:5432/d6ua10v0b8h4lj"
     APP_ID = "1587067"
     API_HASH = "34996e870761c2f772e86ee155079ff5"
     SUDO_USERS = list(set(int(x) for x in ''.split()))
